860aea3e228338cb6abbd37982f44746cd36f770
Joseph <joseph@getmimo.com>
2018-06-06 15:49:04 +0200

    P2: Getting input : Lesson #11 : Finish

--- app/src/main/java/mimo/com/whackamole/MainActivity.kt
+++ app/src/main/java/mimo/com/whackamole/MainActivity.kt
@@ -2,18 +2,39 @@ package mimo.com.whackamole
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var status = "Storage status:"
+        val total = 64.0
+        var used = 64.0
+        var cloud = 5.0
+        var freeSpace = total - used + cloud
+
+        used--
+
+        print(status)
+
+        if (freeSpace == 0.0) {
+            print("Storage full!")
+        } else if (freeSpace <= 1) {
+            print("Storage low")
+        } else {
+            print("More than 1gb available")
+        }
+
+        var networkAvailable : Boolean
+
+        networkAvailable = false
+
+        if (networkAvailable != true) {
+            print("Phone offline")
+        }
+
+
     }
-
-    fun start(startButton: View) {
-        startButton.visibility = View.INVISIBLE
-    }
-
-
 }
