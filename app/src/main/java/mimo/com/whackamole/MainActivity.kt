a5c706f62b681b0176ee7fe4a89cee38ee10da00
Joseph <joseph@getmimo.com>
2018-06-06 08:53:25 +0200

    P2: Lesson #2 : Removing code

--- app/src/main/java/mimo/com/whackamole/MainActivity.kt
+++ app/src/main/java/mimo/com/whackamole/MainActivity.kt
@@ -8,33 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var status = "Storage status:"
-        val total = 64.0
-        var used = 64.0
-        var cloud = 5.0
-        var freeSpace = total - used + cloud
-
-        used--
-
-        print(status)
-
-        if (freeSpace == 0.0) {
-            print("Storage full!")
-        } else if (freeSpace <= 1) {
-            print("Storage low")
-        } else {
-            print("More than 1gb available")
-        }
-
-        var networkAvailable : Boolean
-
-        networkAvailable = false
-
-        if (networkAvailable != true) {
-            print("Phone offline")
-        }
-
-
     }
+
+
 }
