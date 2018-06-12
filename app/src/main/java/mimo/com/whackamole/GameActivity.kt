cf7ed0809ad9b625825854b9e0fb9079ac8c31ef
Joseph <joseph@getmimo.com>
2018-06-12 09:23:35 +0200

    P3: Countdown : #2 Countdown : Make Image Active

--- app/src/main/java/mimo/com/whackamole/GameActivity.kt
+++ app/src/main/java/mimo/com/whackamole/GameActivity.kt
@@ -2,6 +2,7 @@ package mimo.com.whackamole
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.constraint.ConstraintLayout
 import android.widget.ImageButton
 
@@ -14,6 +15,7 @@ class GameActivity : AppCompatActivity() {
 
     fun startGame() {
         val image = pickImage()
+        makeImageActive(image)
     }
 
     fun pickImage() : ImageButton {
@@ -24,5 +26,23 @@ class GameActivity : AppCompatActivity() {
         return randomImage
     }
 
+    fun makeImageActive(image: ImageButton) : CountDownTimer {
+        image.setOnClickListener {
+            image.setImageResource(R.drawable.ic_success)
+        }
 
+        return object : CountDownTimer(1000, 100) {
+
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                image.setImageResource(R.drawable.ic_inactive)
+                image.setOnClickListener(null)
+                startGame()
+            }
+
+        }
+    }
 }
