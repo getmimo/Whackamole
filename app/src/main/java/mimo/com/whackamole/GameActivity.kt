80f0e38f4b41a3f8773bd4d0662d3319992be701
Joseph <joseph@getmimo.com>
2018-06-19 09:35:35 +0200

    P3: New Activity : #1 Hello there

--- app/src/main/java/mimo/com/whackamole/GameActivity.kt
+++ app/src/main/java/mimo/com/whackamole/GameActivity.kt
@@ -2,8 +2,6 @@ package mimo.com.whackamole
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
-import android.widget.ImageButton
 
 class GameActivity : AppCompatActivity() {
 
@@ -11,18 +9,4 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
     }
-
-    fun startGame() {
-        val activeImage = pickImage()
-    }
-
-    fun pickImage() : ImageButton {
-        val images = findViewById<ConstraintLayout>(R.id.gameLayout).touchables
-        images.shuffle()
-        val randomImage = images[0] as ImageButton
-        randomImage.setImageResource(R.drawable.ic_active)
-        return randomImage
-    }
-
-
 }
--- app/src/main/java/mimo/com/whackamole/MainActivity.kt
+++ app/src/main/java/mimo/com/whackamole/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun beginGame(name: String) {
-        startActivity(Intent(this, GameActivity::class.java))
+
     }
 
 
