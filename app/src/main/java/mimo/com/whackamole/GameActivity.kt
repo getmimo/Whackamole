dfe31b5101e24939ab38573e67975e3f2189a97f
Joseph <joseph@getmimo.com>
2018-06-19 09:52:05 +0200

    P3: Countdown : #1 Activate

--- app/src/main/java/mimo/com/whackamole/GameActivity.kt
+++ app/src/main/java/mimo/com/whackamole/GameActivity.kt
@@ -2,6 +2,9 @@ package mimo.com.whackamole
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.support.constraint.ConstraintLayout
+import android.widget.ImageButton
 
 class GameActivity : AppCompatActivity() {
 
@@ -9,4 +12,17 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
     }
+
+    fun startGame() {
+        val image = pickImage()
+    }
+
+    fun pickImage() : ImageButton {
+        val images = findViewById<ConstraintLayout>(R.id.gameLayout).touchables
+        images.shuffle()
+        val randomImage = images[0] as ImageButton
+        randomImage.setImageResource(R.drawable.ic_active)
+        return randomImage
+    }
+
 }
